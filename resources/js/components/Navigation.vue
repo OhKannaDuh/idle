cb270b81c4b43f26a1bff227e080fadcab306331
OhKannaDuh <liammcnair@outlook.com>
2020-10-07 17:58:06 +0100

    added bug report link

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
     <v-navigation-drawer v-model="navigation.show" app floating bottom disable-resize-watcher temporary>
-        <v-list-item link>
+        <v-list-item link href="https://github.com/OhKannaDuh/idle/issues" target="_blank">
             <v-list-item-icon>
                 <v-icon>mdi-bug</v-icon>
             </v-list-item-icon>
